e71adee78c5eac77ec6921673aa6e1ac6652d991
mamba666 <lwjkkbb@sina.com>
2020-08-15 23:03:36 +0800

    8.15尝试接口，明天尝试云函数接口

---
+++ miniprogram/pages/movie/movie.js
@@ -0,0 +1,78 @@
+// pages/movie/movie.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: async function (options) {
+    let {result:res}=await wx.request({
+      url:'http://apis.juhe.cn/simpleWeather/query',
+      data:{
+        city:"北京",
+        key:"9e6e5ce40a497c47a08c8e96af31c76d"
+      },
+      success:res=>{
+        console.log(res)
+      },
+      fail:err=>{
+        console.log(err)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
