7d1912697e585b1bc4a3acc1ea4ab627191a4d75
mamba666 <lwjkkbb@sina.com>
2020-08-18 07:53:16 +0800

    done

--- miniprogram/pages/movie/movie.js
+++ miniprogram/pages/movie/movie.js
@@ -1,27 +1,57 @@
 // pages/movie/movie.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    weatherBJ:{}
   },
-
+  searchBJ:async function(){
+    let res=await db.collection('Weathers').doc('3adec2825f38a7e900cb36525537b1fd').get()
+    this.setData({
+      weatherBJ:res.data
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
-    let {result:res}=await wx.request({
-      url:'http://apis.juhe.cn/simpleWeather/query',
-      data:{
-        city:"北京",
-        key:"9e6e5ce40a497c47a08c8e96af31c76d"
+    await wx.request({
+      url: 'http://apis.juhe.cn/simpleWeather/query',
+      data: {
+        city: "北京",
+        key: "9e6e5ce40a497c47a08c8e96af31c76d"
       },
-      success:res=>{
-        console.log(res)
+      success: res => {
+        let data = res.data
+        console.log(data)
+        db.collection('Weathers').add({
+          data: {
+            reason: data.reason,
+            city: data.result.city,
+            future: {
+              one: {
+                date: data.result.future[0].date,
+                direct: data.result.future[0].direct,
+                temperature: data.result.future[0].temperature,
+                weather: data.result.future[0].weather,
+              },
+              two: {
+                date: data.result.future[1].date,
+                direct: data.result.future[1].direct,
+                temperature: data.result.future[1].temperature,
+                weather: data.result.future[1].weather,
+              }
+            }
+          }
+        })
+        // this.setData({
+        //   dataRes:data
+        // })
       },
-      fail:err=>{
+      fail: err => {
         console.log(err)
       }
     })
